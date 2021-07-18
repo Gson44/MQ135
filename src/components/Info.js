697ed113c545428bf2277bee5b6ea47638358b2e
unknown <geric99997@gmail.com>
2021-07-18 18:32:51 -0500

    first commit

---
+++ src/components/Info.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Info.css'
+const Info = () => {
+    return(
+        <div className="infocontainer">
+            <div className="Info">
+                <h1 className="InfoTitle">Sensor info</h1>
+                <img className="infoImage" src="https://img.icons8.com/nolan/128/info.png"/>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Info
