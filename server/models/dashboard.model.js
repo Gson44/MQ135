264a74f53b4f5a3a18ec87841ba46ca0758a1d43
unknown <geric99997@gmail.com>
2021-08-15 04:58:40 -0500

    Add bluetooth and removing web pages

--- server/models/dashboard.model.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-const DashboardSchema = new mongoose.Schema({
-    userName: {type:String},
-    password: {type: String}
-})
-module.exports.Dashboard = mongoose.model('Dashboard', DashboardSchema)
--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import Data from './views/Data';
-import Main from './views/Main'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Signup from './views/Signup';
+
+
 function App() {
   return (
     <div className="App">
-       <Router>
-       <Switch>
-		      <Route exact path="/" component={Main}/>
-				  <Route exact path="/Signup" component={Signup }/>
-          <Route exact path="/loginin" component={Data} />
-	    </Switch>
-    </Router>
+          <Data /> 
     </div>
   );
 }
