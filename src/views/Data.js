82c387de9595f34cebf657054fc7952d9e5339a7
unknown <geric99997@gmail.com>
2021-08-16 06:47:08 -0500

    add gauge

--- src/views/Data.js
+++ src/views/Data.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
-import Info from '../components/Info'
 import UI from '../components/UI'
 const Data = () => {
     return(
         <div>
             <Header />
             <UI/>
-            {/* <Info /> */}
+            
         </div>
     )
 }
